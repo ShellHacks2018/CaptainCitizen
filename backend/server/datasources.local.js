5a588d2454a7789e7762d6cb048f4719c1aeaf6c
Mauricio <msant323@fiu.edu>
2018-09-15 17:32:21 -0400

    add mapitems model to backend'

--- backend/server/datasources.local.js
+++ backend/server/datasources.local.js
@@ -11,10 +11,19 @@ module.exports = {
     username: envs.db_user,
     password: envs.db_pw,
   },
+  mapitems: {
+    url: envs.db_url,
+    port: 27017,
+    database: 'mapitems',
+    name: 'mapitems',
+    connector: 'mongodb',
+    username: envs.db_user,
+    password: envs.db_pw,
+  },
   s3: {
-    name: "s3",
-    connector: "loopback-component-storage",
-    provider: "amazon",
+    name: 's3',
+    connector: 'loopback-component-storage',
+    provider: 'amazon',
     key: envs.aws_key,
     keyId: envs.aws_keyid
   }
