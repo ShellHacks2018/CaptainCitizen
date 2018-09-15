a2eabb652a01c98668e70b1a22c9e11c0d52972d
Amy <amy.sudarat@gmail.com>
2018-09-15 01:20:37 -0400

    delete package-log.json and add build.py

---
+++ backend/build.py
@@ -0,0 +1,17 @@
+import shutil
+import os
+
+
+def copyTree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
+
+if __name__ == "__main__":
+    src = r"C:\CaptainCitizen\frontend\build"
+    dst = r"C:\CaptainCitizen\backend\client"
+    copyTree(src, dst)
