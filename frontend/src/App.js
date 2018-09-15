d27a45349d3e10c4fe7176c86a88394fb29df4a5
CristianCepeda <ccepe014@fiu.edu>
2018-09-15 00:40:54 -0400

    Initial Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import LandingPC from './Pages/LandingPC'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/' component={LandingPC} />
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
-export default App;
+export default App
---
+++ frontend/src/Pages/LandingPC.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LandingPC extends Component {
+  render () {
+    return (
+      <div>
+        <h2>Whats up Guys</h2>
+      </div>
+    )
+  }
+}
+
+export default LandingPC
