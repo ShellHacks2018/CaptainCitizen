c5d95c2b2ce86a5100d5cdfd55d432b49e306bd5
asalinasj <andressalinasj@gmail.com>
2018-09-16 02:44:32 -0400

    branch
    branch

---
+++ backend/server/config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    db_url: "mongodb+srv://heroku2018:password2018@moodjournal-voeew.mongodb.net/cc?retryWrites=true",
+    db_users: "heroku2018",
+    db_pw: "password2018",
+    aws_key: "MJYfuztvYNDDEmWC9q3H91EGk5ePxP7EKF1T4+gR",
+    aws_keyid: "AKIAJQHLOZL46NVQBHGQ"
+};
