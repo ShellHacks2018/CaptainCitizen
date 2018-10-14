a636418fcc48b61bd0d71fe9e2318ae4979dcc9f
amysudarat <amy.sudarat@gmail.com>
2018-10-14 16:38:19 -0700

    commit changes in yarn.lock

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,8 +11,8 @@ class App extends Component {
         <div className="App">
           <NavBarC />
           <Switch>
-            <Route exact path="/" component={LandingPC} />
-            <Route path="/home" component={HomePC} />
+            <Route exact path="/" component={HomePC} />
+            <Route path="/home" component={LandingPC} />
           </Switch>
         </div>
       </Router>
